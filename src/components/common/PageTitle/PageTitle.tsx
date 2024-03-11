a6195212841a07af6f22d12b2f64e507a9eb4559
happy-cutman <nikita.silkin@dexola.com>
2024-03-11 12:23:40 +0200

    FIX | build

--- src/components/common/PageTitle/PageTitle.tsx
+++ src/components/common/PageTitle/PageTitle.tsx
@@ -11,7 +11,7 @@ export const PageTitle = (props: PageTitleProps) => {
 
   return (
     <div>
-      <AppLink to={'/'} className={'mb-8 gap-2 normal-case'}>
+      <AppLink to={'/'} variant={'ghost'} className={'mb-8 gap-2 normal-case'}>
         <ChevronLeft className="h-5 w-5 mt-[2px]"/>
         Home page
       </AppLink>
