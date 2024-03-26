50d30a674aca63447d7ede4300b87666bdde12aa
happy-cutman <nikita.silkin@dexola.com>
2024-03-26 11:29:25 +0200

    FIX | nft card link

--- src/components/widgets/cards/NftCard/NftCard.tsx
+++ src/components/widgets/cards/NftCard/NftCard.tsx
@@ -23,43 +23,42 @@ export const NftCard = (props: NftCardProps) => {
   const solanaIcons = ['SOL', 'JUP', 'RNDR', 'HNT', 'BONK', 'PYTH', 'RAY', 'JTO', 'WIF']
 
   return (
-    <Link to={`${title}/${id}`}>
-      <Card>
-        <img
-          src={defaultCard}
-          style={{ width: 'calc(100% + 48px)', maxWidth: 'none' }}
-          className={'rounded-md mb-6 -ml-[24px] -mt-[24px] rounded-b-none'}
-        />
+    <Card className={'relative'}>
+      <Link to={`${title}/${id}`} className={'z-10 absolute w-full h-full top-0 left-0'} />
+      <img
+        src={defaultCard}
+        style={{ width: 'calc(100% + 48px)', maxWidth: 'none' }}
+        className={'rounded-md mb-6 -ml-[24px] -mt-[24px] rounded-b-none'}
+      />
 
-        <CardHeader>
-          <CardTitle>{title}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className={'flex items-center gap-2 mb-4'}>
-            {title === 'classic'
-              ? classicIcons.map((icon) => <img key={icon} className={'h-6 w-6 -mt-[3px]'} src={currencyIcons[icon]} />)
-              : solanaIcons.map((icon) => <img key={icon} className={'h-6 w-6 -mt-[3px]'} src={currencyIcons[icon]} />)}
-          </div>
-          <div className={'flex items-center justify-between'}>
-            <span className={'font-regular text-sm text-card-additionalForeground'}>Invested</span>
-            <span className={'font-roboto text-sm font-medium'}>{content?.invested}</span>
-          </div>
-          <div className={'flex items-center justify-between'}>
-            <span className={'font-regular text-sm text-card-additionalForeground'}>Current Price</span>
-            <span className={'font-roboto text-sm font-medium'}>{content.price}</span>
-          </div>
-        </CardContent>
-        <CardFooter className={'flex gap-4 pt-6'}>
-          <Button className={'flex-1 gap-2'} variant={'accent'}>
-            <ArrowUpDown className={'w-4 h-4'} />
-            Transfer
-          </Button>
-          <Button className={'flex-1 gap-2'} variant={'destructive'} onClick={() => setModalName('BURN_NFT')}>
-            <Flame className={'w-4 h-4'} />
-            Burn
-          </Button>
-        </CardFooter>
-      </Card>
-    </Link>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className={'flex items-center gap-2 mb-4'}>
+          {title === 'classic'
+            ? classicIcons.map((icon) => <img key={icon} className={'h-6 w-6 -mt-[3px]'} src={currencyIcons[icon]} />)
+            : solanaIcons.map((icon) => <img key={icon} className={'h-6 w-6 -mt-[3px]'} src={currencyIcons[icon]} />)}
+        </div>
+        <div className={'flex items-center justify-between'}>
+          <span className={'font-regular text-sm text-card-additionalForeground'}>Invested</span>
+          <span className={'font-roboto text-sm font-medium'}>{content?.invested}</span>
+        </div>
+        <div className={'flex items-center justify-between'}>
+          <span className={'font-regular text-sm text-card-additionalForeground'}>Current Price</span>
+          <span className={'font-roboto text-sm font-medium'}>{content.price}</span>
+        </div>
+      </CardContent>
+      <CardFooter className={'flex gap-4 pt-6 relative z-20'}>
+        <Button className={'flex-1 gap-2'} variant={'accent'}>
+          <ArrowUpDown className={'w-4 h-4'} />
+          Transfer
+        </Button>
+        <Button className={'flex-1 gap-2'} variant={'destructive'} onClick={() => setModalName('BURN_NFT')}>
+          <Flame className={'w-4 h-4'} />
+          Burn
+        </Button>
+      </CardFooter>
+    </Card>
   )
 }
