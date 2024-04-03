51a85acdf40bdf977c92e4f772b50fc1f6ce6dad
DaniilPopovDexola <daniil.popov@dexola.com>
2024-04-03 19:30:33 +0300

    FIX | NaN on FormCurrency

--- src/components/common/FormCurrency/lib.ts
+++ src/components/common/FormCurrency/lib.ts
@@ -1,4 +1,7 @@
 export const formatCurrencyNumber = (number: number) => {
+  if(isNaN(number)){
+    return 0
+  }
   const formatter = Intl.NumberFormat('en', {
     notation: 'compact',
     // style: 'currency',
